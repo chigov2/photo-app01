e5a6699030ac8a143e33577256d0fc8dac85ee0a
chigov2 <chigov@gmail.com>
2022-07-26 15:44:55 +0300

    9-11-full

--- src/main/java/com/spring/mvc/security/SecurityConstants.java
+++ src/main/java/com/spring/mvc/security/SecurityConstants.java
@@ -1,5 +1,7 @@
 package com.spring.mvc.security;
 
+import com.spring.mvc.SpringApplicationContext;
+
 public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864000000; // 10 days
     public static final long PASSWORD_RESET_EXPIRATION_TIME = 3600000; // 1 hour
@@ -10,11 +12,11 @@ public class SecurityConstants {
     public static final String PASSWORD_RESET_REQUEST_URL = "/users/password-reset-request";
     public static final String PASSWORD_RESET_URL = "/users/password-reset";
     public static final String H2_CONSOLE = "/h2-console/**";
-    public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";
 
-//    public static String getTokenSecret()
-//    {
-//        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
-//        return appProperties.getTokenSecret();
-//    }
+
+    public static String getTokenSecret()
+    {
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+        return appProperties.getTokenSecret();
+    }
 }
