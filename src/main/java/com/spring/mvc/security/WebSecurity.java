bf0ce93876d5506b5c1fc774b401a5685703b968
Max2 <chigov@gmail.com>
2022-07-26 11:39:58 +0300

    9-7

--- src/main/java/com/spring/mvc/security/WebSecurity.java
+++ src/main/java/com/spring/mvc/security/WebSecurity.java
@@ -26,12 +26,23 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
-
+    @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST,SecurityConstants.SIGN_UP_URL)
                 .permitAll()
-                .anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+                .anyRequest().authenticated().and().addFilter(getAuthenticationFilter());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    protected AuthenticationFilter getAuthenticationFilter() throws Exception {
+        final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+        filter.setFilterProcessesUrl("/users/login");
+        return filter;
     }
 
 //    @Override
@@ -60,17 +71,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 //        http.headers().frameOptions().disable();
 //    }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
-//
-//    protected AuthenticationFilter getAuthenticationFilter() throws Exception {
-//        final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
-//        filter.setFilterProcessesUrl("/users/login");
-//        return filter;
-//    }
-//
 //    @Bean
 //    public CorsConfigurationSource corsConfigurationSource()
 //    {
